c8309f6f5ded00aa03b549414acce6d616905afc
Latoya Jackson <jacksonlatoya04@gmail.com>
2021-09-16 16:49:41 -0500

    git pull

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login', [LoginController::class, 'login']);
+
 

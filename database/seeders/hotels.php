7da922433f04fcd73759847ebeb42de21bce0222
Latoya Jackson <jacksonlatoya04@gmail.com>
2021-09-16 21:26:07 -0500

    updated hotel seeder

--- database/seeders/hotels.php
+++ database/seeders/hotels.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class hotels extends Seeder
+class Hotels extends Seeder
 {
     /**
      * Run the database seeds.
@@ -14,20 +14,21 @@ class hotels extends Seeder
     public function run()
     {
         //
-        \App\Models\hotels::truncate();
-        \App\Models\hotels::create(['Hotel_ID' => 'GBPJ', 'Hotel_Name' => 'Gran Bahia Principe Jamaica Hotel', 'Hotel_Location'=>'Montego Bay']);
-        \App\Models\hotels::create(['Hotel_ID' => 'GLBH', 'Hotel_Name' => 'Grand Lido Braco Hotel', 'Hotel_Location' => 'Montego Bay']);
+        \App\Models\hotel::truncate();
+        \App\Models\hotel::create(['Hotel_ID' => 'GBPJ', 'Hotel_Name' => 'Gran Bahia Principe Jamaica Hotel', 'Hotel_Location'=>'Montego Bay']);
 
-        \App\Models\hotels::create(['Hotel_ID' => 'STBR', 'Hotel_Name' => 'Starfish Trelawny Beach Resort', 'Hotel_Location' => 'Falmouth']);
+        \App\Models\hotel::create(['Hotel_ID' => 'GLBH', 'Hotel_Name' => 'Grand Lido Braco Hotel', 'Hotel_Location' => 'Montego Bay']);
 
-        App\Models\hotels::create(['Hotel_ID' => 'FDRP', 'Hotel_Name' => 'FDR Pebbles Hotel ', 'Hotel_Location' => 'Falmouth']);
+        \App\Models\hotel::create(['Hotel_ID' => 'STBR', 'Hotel_Name' => 'Starfish Trelawny Beach Resort', 'Hotel_Location' => 'Falmouth']);
 
-        \App\Models\hotels::create(['Hotel_ID' => 'CSCR', 'Hotel_Name' => 'Coral Seas Cliff Resort ', 'Hotel_Location' => 'Negril']);
+        \App\Models\hotel::create(['Hotel_ID' => 'FDRP', 'Hotel_Name' => 'FDR Pebbles Hotel', 'Hotel_Location' => 'Falmouth']);
 
-        \App\Models\hotels::create(['Hotel_ID' => 'CSAH', 'Hotel_Name' => 'Couples Swept Away Hotel ', 'Hotel_Location' => 'Negril']);
+        \App\Models\hotel::create(['Hotel_ID' => 'CSCR', 'Hotel_Name' => 'Coral Seas Cliff Resort ', 'Hotel_Location' => 'Negril']);
 
-        \App\Models\hotels::create(['Hotel_ID' => 'HIII', 'Hotel_Name' => 'Hedonism III Resort', 'Hotel_Location' => 'Ocho Rios']);
+        \App\Models\hotel::create(['Hotel_ID' => 'CSAH', 'Hotel_Name' => 'Couples Swept Away Hotel ', 'Hotel_Location' => 'Negril']);
 
-        \App\Models\hotels::create(['Hotel_ID' => 'BRZH', 'Hotel_Name' => 'Breezes Hotel', 'Hotel_Location' => 'Ocho Rios']);
+        \App\Models\hotel::create(['Hotel_ID' => 'HIII', 'Hotel_Name' => 'Hedonism III Resort', 'Hotel_Location' => 'Ocho Rios']);
+
+        \App\Models\hotel::create(['Hotel_ID' => 'BRZH', 'Hotel_Name' => 'Breezes Hotel', 'Hotel_Location' => 'Ocho Rios']);
     }
 }

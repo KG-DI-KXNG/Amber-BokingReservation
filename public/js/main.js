21b2ba7d882873a45cc92a1cb027a3705b3c36f3
KG-DI-KXNG <kevandogordon@yahoo.com>
2021-09-17 10:31:54 -0500

    Pay now init

--- public/js/main.js
+++ public/js/main.js
@@ -277,6 +277,11 @@
 
 	};
 	
+	var dateForm = function() {
+		$('#date-start').datepicker({startDate: '1d',endDate: '+2m'});
+		$('#dob').datepicker({startDate: '-70y',endDate: '-12y'});
+
+	};
 
 	var parallax = function() {
 		$(window).stellar({
@@ -286,6 +291,49 @@
 
 		});
 	};
+
+
+
+
+       document.addEventListener("alpine:init", () => {
+        Alpine.data("creditCard", () => ({
+          init() {
+            console.log('Component mounted');
+          },
+          format() {
+            if (this.cardNumber.length > 18) {
+              return;
+            }
+            this.cardNumber = this.cardNumber.replace(/\W/gi, '').replace(/(.{4})/g, '$1 ');
+          },
+          get isValid() {
+            if (this.cardholder.length < 5) {
+              return false;
+            }
+            if (this.cardNumber === '') {
+              return false;
+            }
+            if (this.expired.month === '' && this.expired.year === '') {
+              return false;
+            }
+            if (this.securityCode.length !== 3) {
+              return false;
+            }
+            return true;
+          },
+          onSubmit() {
+            alert(`You did it ${this.cardholder}.`);
+          },
+          cardholder: '',
+          cardNumber: '',
+          expired: {
+            month: '',
+            year: '',
+          },
+          securityCode: '',
+          card: 'front',
+        }));
+      });
 
 
 	
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,6 +48,10 @@
 	<!-- Theme style  -->
 	<link rel="stylesheet" href="css/style.css">
 
+	<!-- Javascript for popup pay now -->
+	<script defer src="https://unpkg.com/alpinejs@3.2.2/dist/cdn.min.js"></script>
+
+	<!-- Tailwind Css -->
 	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
 	<!-- Modernizr JS -->
@@ -147,7 +151,6 @@
 																<option>--No Activity--</option>
 															@endforelse
 															
-															
 														</select>
 													</div>
 												</div>
@@ -186,10 +189,10 @@
 
 												<div class="row form-group">
 													<div class="col-md-12">
-														<input type="submit" class="btn btn-primary btn-block" value="Submit">
+														<input data-toggle="modal" data-target="#pay" class="btn btn-primary btn-block" value="Submit">
 													</div>
 												</div>
-											</form>	
+											
 										</div>
 
 										
@@ -204,6 +207,167 @@
 			</div>
 		</div>
 	</header>
+
+	<!-- Popup Pay Now Card -->
+
+	<div class="modal fade" id="pay" tabindex="-1" role="dialog" aria-labelledby="pay" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+		  <div class="modal-content">
+			
+			  
+			  
+			</div>
+			<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+				<span aria-hidden="true" class="text-red-500 text-9xl ">&times;</span>
+			  </button>            
+          </div>
+      
+          <div class="m-4">
+			<div class="credit-card w-full sm:w-auto shadow-lg mx-auto rounded-xl bg-white" x-data="creditCard">
+			  <header class="flex flex-col justify-center items-center">
+				<div
+				  class="relative"
+				  x-show="card === 'front'"
+				  x-transition:enter="transition ease-out duration-300"
+				  x-transition:enter-start="opacity-0 transform scale-90"
+				  x-transition:enter-end="opacity-100 transform scale-100"
+				>
+				  <img class="w-full h-auto" src="https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/svg-cards/card-visa-front.png" alt="front credit card">
+				  <div class="front bg-transparent text-lg w-full text-white px-12 absolute left-0 bottom-12">
+					<p class="number mb-5 sm:text-xl" x-text="cardNumber !== '' ? cardNumber : '0000 0000 0000 0000'"></p>
+					<div class="flex flex-row justify-between">
+					  <p x-text="cardholder !== '' ? cardholder : 'Card holder'"></p>
+					  <div class="">
+						<span x-text="expired.month"></span>
+						<span x-show="expired.month !== ''">/</span>
+						<span x-text="expired.year"></span>
+					  </div>
+					</div>
+				  </div>
+				</div>
+				<div
+				  class="relative"
+				  x-show="card === 'back'"
+				  x-transition:enter="transition ease-out duration-300"
+				  x-transition:enter-start="opacity-0 transform scale-90"
+				  x-transition:enter-end="opacity-100 transform scale-100"
+				>
+				  <img class="w-full h-auto" src="https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/svg-cards/card-visa-back.png" alt="">
+				  <div
+					class="bg-transparent text-white text-xl w-full flex justify-end absolute bottom-20 px-8  sm:bottom-24 right-0 sm:px-12"
+				  >
+					<div class="border border-white w-16 h-9 flex justify-center items-center">
+					  <p x-text="securityCode !== '' ? securityCode : 'code'"></p>
+					</div>
+				  </div>
+				</div>
+				<ul class="flex">
+				  <li class="mx-2">
+					<img class="w-16" src="https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/computop.png" alt="" />
+				  </li>
+				  <li class="mx-2">
+					<img class="w-14" src="https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/verified-by-visa.png" alt="" />
+				  </li>
+				  <li class="ml-5">
+					<img class="w-7" src="https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/mastercard-id-check.png" alt="" />
+				  </li>
+				</ul>
+			  </header>
+			  <main class="mt-4 p-4">
+				<h1 class="text-xl font-semibold text-gray-700 text-center">Card payment</h1>
+				<div class="">
+				  <div class="my-3">
+					<input
+					  type="text"
+					  class="block w-full px-5 py-2 border rounded-lg bg-white shadow-lg placeholder-gray-400 text-gray-700 focus:ring focus:outline-none"
+					  name="card_holder"
+					  placeholder="Card holder"
+					  maxlength="22"
+					  x-model="cardholder"
+					/>
+				  </div>
+				  <div class="my-3">
+					<input
+					  type="text"
+					  class="block w-full px-5 py-2 border rounded-lg bg-white shadow-lg placeholder-gray-400 text-gray-700 focus:ring focus:outline-none"
+					  name="card_number"
+					  placeholder="Card number"
+					  x-model="cardNumber"
+					  x-on:keydown="format()"
+					  x-on:keyup="isValid()"
+					  maxlength="19"
+					/>
+				  </div>
+				  <div class="my-3 flex flex-col">
+					<div class="mb-2">
+					  <label for="" class="text-gray-700">Expired</label>
+					</div>
+					<div class="grid grid-cols-2 sm:grid-cols-4 gap-2">
+					  <select
+						id=""
+						name="card_month"
+						class="form-select appearance-none block w-full px-5 py-2 border rounded-lg bg-white shadow-lg placeholder-gray-400 text-gray-700 focus:ring focus:outline-none"
+						x-model="expired.month"
+					  >
+						<option value="" selected disabled>MM</option>
+						<option value="01">01</option>
+						<option value="02">02</option>
+						<option value="03">03</option>
+						<option value="04">04</option>
+						<option value="05">05</option>
+						<option value="06">06</option>
+						<option value="07">07</option>
+						<option value="08">08</option>
+						<option value="09">09</option>
+						<option value="10">10</option>
+						<option value="11">11</option>
+						<option value="12">12</option>
+					  </select>
+					  <select
+						name="card_year"
+						id=""
+						class="form-select appearance-none block w-full px-5 py-2 border rounded-lg bg-white shadow-lg placeholder-gray-400 text-gray-700 focus:ring focus:outline-none"
+						x-model="expired.year"
+					  >
+						<option value="" selected disabled>YY</option>
+						<option value="2021">2021</option>
+						<option value="2022">2022</option>
+						<option value="2023">2023</option>
+						<option value="2024">2024</option>
+						<option value="2025">2025</option>
+						<option value="2026">2026</option>
+					  </select>
+					  <input
+						type="text"
+						name="card_cvv"
+						class="block w-full col-span-2 px-5 py-2 border rounded-lg bg-white shadow-lg placeholder-gray-400 text-gray-700 focus:ring focus:outline-none"
+						placeholder="Security code"
+						maxlength="3"
+						x-model="securityCode"
+						x-on:focus="card = 'back'"
+						x-on:blur="card = 'front'"
+					  />
+					</div>
+				  </div>
+				</div>
+			  </main>
+			  <footer class="mt-6 p-4">
+				<button
+				  class="submit-button px-4 py-3 rounded-full bg-blue-300 text-blue-900 focus:ring focus:outline-none w-full text-xl font-semibold transition-colors"
+				  x-bind:disabled="!isValid"
+				  x-on:click="onSubmit()"
+				>
+				  Pay now
+				</button>
+			  </footer>
+			</div>
+		  </div>
+
+        </form>
+        </div>
+      </div>  
+	  
+	 
 	
 	<div class="gtco-section">
 		<div class="gtco-container">
@@ -345,6 +509,14 @@
 	</div>
 
 
+	
+	 
+	  
+
+
+
+
+      
 	
 	<footer id="gtco-footer" role="contentinfo">
 		<div class="gtco-container">
